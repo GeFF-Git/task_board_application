b6a658ead85b4b04119cafc08f7d96aa76e7f1d2
Geff <geffyjebasekar@gmail.com>
2026-02-26 07:05:27 +0530

    Sub brand name changes

--- task-board-ui/src/widgets/sidebar/sidebar.component.ts
+++ task-board-ui/src/widgets/sidebar/sidebar.component.ts
@@ -20,7 +20,7 @@ import { TaskStore } from '@entities/task/api/task.store';
           @if (!collapsed()) {
             <div class="brand-info">
               <span class="brand-name">Relate<em>wise</em></span>
-              <span class="brand-sub">microdose.studio</span>
+              <span class="brand-sub">Application</span>
             </div>
           }
         </div>
